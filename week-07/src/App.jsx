3fc47b83a3f2485f259b636e6d74fc837dcb0f4e
Alle Manoj <22211a1205@bvrit.ac.in>
2025-02-16 14:58:27 +0530

    first_week_07

---
+++ week-07/src/App.jsx
@@ -0,0 +1,38 @@
+import { useContext, useState } from "react"
+import { CountContext } from "./context";
+
+
+function App() {
+  const [count,setCount] = useState(0);
+  return (
+  <div>
+    <CountContext.Provider value={count}>
+      <Count count={count} setCount = {setCount} />
+    </CountContext.Provider>
+   </div>
+  )
+}
+function Count({setCount}) {
+  return <div>
+    <CountRerender/>
+    <Buttons setCount = {setCount}/>
+  </div>
+}
+function CountRerender() {
+  const count = useContext(CountContext);
+  return <div>
+      {count}
+  </div>
+}
+function Buttons({setCount}) {
+  const count = useContext(CountContext);
+    return <div>
+      <button onClick={()=>{
+          setCount(count+1);
+      }}>Increment</button>
+      <button onClick={()=>{
+          setCount(count-1);
+      }}>Dcrement</button>
+    </div>
+}
+export default App
