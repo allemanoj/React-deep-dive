bb3172d53654fdc66fb929134643e360be4bc7cc
Alle Manoj <22211a1205@bvrit.ac.in>
2025-02-20 19:16:26 +0530

    Recoil stuff understand hooks

---
+++ week-07-recoil/recoil-p/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from './atoms'
+
+function App() {
+  
+  return (<RecoilRoot>
+    <MainApp/>
+  </RecoilRoot>
+  )
+}
+function MainApp() {
+  const newtWorkNotifications = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationAtomCount = useRecoilValue(notificationAtom);
+  const messagingAtomCount = useRecoilValue(messagingAtom);
+return (
+    <>
+     <button>Home</button>
+
+     <button>Jobs ({jobsAtomCount})</button>
+     <button>My Network ({newtWorkNotifications >= 100 ? "99+" : newtWorkNotifications})</button>
+     <button>Messageing ({messagingAtomCount})</button>
+     <button>Notifications ({notificationAtomCount})</button>
+      <ButtonUpdater/>
+    </>
+  )
+}
+function ButtonUpdater() {
+  const setMessagingAtomgCount = useSetRecoilState(messagingAtom);
+  return <button onClick={()=>{
+      setMessagingAtomgCount(c => c + 1);
+     }}>Me</button>
+}
+
+export default App
